38fea98de607dda4ea068d92fe86c5808745e4a0
Sasha Doubov <sasha@mosaicml.com>
2023-10-28 03:02:15 +0000

    ad

--- composer/utils/checkpoint.py
+++ composer/utils/checkpoint.py
@@ -487,7 +487,7 @@ def load_sharded_checkpoint(
                 # Call function to modify state_dict
                 ignore_keys(model_state_dict)
 
-            print(model_state_dict.keys())
+            print(model_state_dict['state']['model'].keys())
 
             dist_cp.load_state_dict(model_state_dict, storage_reader, planner=RenameLoadPlanner(state.model))
 
