0a597f676982eff9d25d8a87d0bac5e593647abd
Sasha Doubov <sasha@mosaicml.com>
2024-01-04 19:46:28 +0000

    change to gate on dev

--- composer/trainer/mosaic_fsdp.py
+++ composer/trainer/mosaic_fsdp.py
@@ -54,7 +54,7 @@ def patch_pytorch():
         from torch.distributed.fsdp import _runtime_utils
         _runtime_utils._validate_and_get_hybrid_shard_state = lambda *args, **kwargs: None
 
-    elif version.parse(torch.__version__) < version.parse('2.2.0'):
+    elif version.parse(torch.__version__) < version.parse('2.2.0.dev'):
         # Monkey patch for torch < 2.2.0 ie torch == 2.1.1, 2.1.2
 
         # Allow 2D HSDP
